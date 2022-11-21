f592f82b643e7e8f0ead8f1f945f3264e40991af
sam-thewise <sam@ticket-it.com>
2022-11-22 11:25:36 +1300

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import { QueryClient } from "react-query"
 
-let medusaUrl = "http://localhost:9000"
+let medusaUrl = "https://fuji-medusa.herokuapp.com"
 
 // deprecated
 if (process.env.GATSBY_STORE_URL) {
