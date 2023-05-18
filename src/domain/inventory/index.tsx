e3bcdf9a0b97931ace6ce9f32e18038639731237
sam-thewise <sam@ticket-it.com>
2023-05-18 12:06:42 +1200

    updating build error

--- src/domain/inventory/index.tsx
+++ src/domain/inventory/index.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import InventoryView from "./inventory"
-import Locations from "./locations"
+// import Locations from "./locations"
 
 const Inventory = () => {
   return (
     <Routes>
       <Route index element={<InventoryView />} />
-      <Route path="/locations/*" element={<Locations />} />
+      {/* <Route path="/locations/*" element={<Locations />} /> */}
     </Routes>
   )
 }
